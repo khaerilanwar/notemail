5a217f41f5ac70c56cefcf0bb672f6a4807fee6a
khaerilanwar <khaerilanwar1992@gmail.com>
2024-08-28 14:29:52 +0700

    fix bug auth

--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -2,24 +2,8 @@ import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 import { createContext } from "react";
 import Swal from "sweetalert2";
-import { useState } from "react";
 
-// const baseURL = "https://notemails-e6d0a4f82467.herokuapp.com/"
-const baseURL = "http://localhost:3000/"
-
-let isRefreshing = false;
-let refreshSubscribers = [];
-
-// Fungsi untuk menambahkan subscriber ke antrian
-function subscribeTokenRefresh(cb) {
-    refreshSubscribers.push(cb);
-}
-
-// Fungsi untuk mengosongkan antrian dan mengirim ulang semua permintaan yang menunggu
-function onRefreshed(token) {
-    refreshSubscribers.map((cb) => cb(token));
-    refreshSubscribers = [];
-}
+const baseURL = "http://localhost:3000"
 
 const ApiContext = createContext()
 const axiosInstance = axios.create({
@@ -28,68 +12,84 @@ const axiosInstance = axios.create({
 })
 
 axiosInstance.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
+    async (config) => {
+        // Mendapatkan token yang disimpan di local storage
+        const token = localStorage.getItem("token")
         if (token) {
-            config.headers['Authorization'] = 'Bearer ' + token;
+            // Mengecek apakah token sudah kadaluarsa
+            const expiration = jwtDecode(token).exp * 1000
+            if (Date.now() >= expiration) {
+                localStorage.removeItem("token")
+                try {
+                    const response = await axiosInstance.get("/token")
+                    const newToken = response.data.accessToken
+                    localStorage.setItem("token", newToken)
+                    console.log("Dapet token baru!")
+                    config.headers.Authorization = `Bearer ${newToken}`
+                } catch (error) {
+                    console.error("Gagal mendapatkan token!", error)
+                    localStorage.removeItem("token")
+                    throw error
+                }
+            } else {
+                config.headers.Authorization = `Bearer ${token}`
+            }
         }
-        return config;
+        return config
     },
     (error) => {
-        return Promise.reject(error);
+        return Promise.reject(error)
     }
 )
 
 axiosInstance.interceptors.response.use(
     (response) => {
-        return response;
+        return response
     },
-    (error) => {
-        const { config, response } = error;
-        const originalRequest = config;
+    async (error) => {
+        const originalRequest = error.config
 
-        if (response.status === 401 && !originalRequest._retry) {
-            if (!isRefreshing) {
-                isRefreshing = true;
-                originalRequest._retry = true;
-
-                return new Promise((resolve, reject) => {
-                    axios
-                        .get('/token') // Endpoint untuk memperbarui token
-                        .then(({ data }) => {
-                            localStorage.setItem('token', data.accessToken);
-                            axiosInstance.defaults.headers['Authorization'] =
-                                'Bearer ' + data.accessToken;
-                            onRefreshed(data.accessToken);
-                            resolve(api(originalRequest));
-                        })
-                        .catch((err) => {
-                            reject(err);
-                        })
-                        .finally(() => {
-                            isRefreshing = false;
-                        });
-                });
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true
+            try {
+                const response = await axiosInstance.get("/token")
+                const newToken = response.data.accessToken
+                localStorage.setItem("token", newToken)
+                console.log("Dapet token baru!")
+                axios.defaults.headers['Authorization'] = `Bearer ${newToken}`
+                originalRequest.headers['Authorization'] = `Bearer ${newToken}`
+                return axiosInstance(originalRequest)
+            } catch (error) {
+                console.error("Gagal dapat token baru!", e)
+                return Promise.reject(error)
             }
+        } else {
+            // Sweet Alert
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top",
+                showConfirmButton: false,
+                timer: 4000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            });
 
-            // Menunggu pembaruan token selesai
-            return new Promise((resolve) => {
-                subscribeTokenRefresh((token) => {
-                    originalRequest.headers['Authorization'] = 'Bearer ' + token;
-                    resolve(axiosInstance(originalRequest));
-                });
+            Toast.fire({
+                icon: "error",
+                title: error.response.data.message
             });
         }
 
-        return Promise.reject(error);
+        return Promise.reject(error)
     }
 )
 
 function ApiProvider({ children }) {
-    const [messageApi, setMessageApi] = useState()
-
     return (
-        <ApiContext.Provider value={{ api: axiosInstance, baseURL, messageApi, setMessageApi }}>
+        <ApiContext.Provider value={{ api: axiosInstance, baseURL }}>
             {children}
         </ApiContext.Provider>
     )
